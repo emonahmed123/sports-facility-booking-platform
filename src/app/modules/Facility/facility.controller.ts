8413e93ad9c09bffecb94aac51a6a15496cf8fb1
Emon Ahmed <imonshomon@gmail.com>
2024-08-13 22:54:33 +0600

    done

--- src/app/modules/Facility/facility.controller.ts
+++ src/app/modules/Facility/facility.controller.ts
@@ -20,7 +20,7 @@ const getAllFacility = catchAsync(async (req: Request, res: Response) => {
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'Facilities retrieved successfully',
+    message:'Facilities retrieved successfully',
     data: result,
   });
 });
